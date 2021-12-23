79babcf15c5257c47c81af4e0ece79737081ef27
Sam Phung <sphung@uncc.edu>
2021-12-23 18:33:42 -0500

    Change base url to heroku url

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,5 +3,5 @@ import axios from 'axios'
 export const wesourceBackend = axios.create({
     // baseURL:"https://3.128.198.193:8080/api/"
     /// Development
-    baseURL: "http://127.0.0.1:5000/api/"
+    baseURL: "https://wesource-web-app.herokuapp.com/api/"
 });
